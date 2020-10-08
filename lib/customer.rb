92b4040b95b8e85eaf79d3f55e3c53a4b24340bf
Victorsloop <torres.victor136@gmail.com>
2020-10-08 16:36:03 -0400

    finished meal

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
 class Customer
-  
+
+    attr_accessor :name , :age 
+    @@all = [] 
+
+    def initialize(name,age)
+        @name = name 
+        @age = age 
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def new_meal(waiter, total, tip)
+        food = Meal.new(waiter, self, total ,tip)
+    end 
+
+    def meals 
+        Meal.all.select{|food| food.customer == self }
+    end 
+
+    def waiters 
+        meals.map {|homie| homie.waiter}
+    end 
 end
